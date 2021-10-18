b08cfe30098640c47e2ff0b0ff05881b38299b07
CarmerciJ <carmercijimenez@gmail.com>
2021-10-18 17:15:14 -0400

    adding finished changes to make both tests pass

--- src/main/java/com/upgrade/pages/LoginInfoPage.java
+++ src/main/java/com/upgrade/pages/LoginInfoPage.java
@@ -4,6 +4,7 @@ import com.upgrade.pojos.Borrower;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
 
@@ -33,19 +34,26 @@ public class LoginInfoPage extends BasePage {
     @FindBy(name = "agreements")
     private WebElement agreements;
 
+
     public LoginInfoPage(WebDriver driver) {
         super(driver);
         waitForWebElements(Arrays.asList(email));
     }
 
     // Note : Use java generics to return a different page
-    public SelectOfferPage enterLoginDetails(Borrower randomPerson) {
+    //updated method to include generics so that this method is
+    // accessible from multiple pages but returns the instance of same page object on which it is called
+    public <T extends BasePage> T enterLoginDetails(Borrower randomPerson, T page) {
         type(email, randomPerson.getEmail());
+        pause(4);
         type(password, randomPerson.getPassword());
+        pause(4);
         selectTermsOfUse();
+        pause(4);
         click(checkYourRateBtn);
         waitForPage();
-        return new SelectOfferPage(driver);
+        return page;
+
     }
 
     public LoginInfoPage selectTermsOfUse() {
